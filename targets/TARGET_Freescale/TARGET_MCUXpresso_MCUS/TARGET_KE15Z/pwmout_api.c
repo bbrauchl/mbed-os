6ccaf34ddafcab638812193ae345b2615b0b7f4f
Bryan Brauchler <bbrauchl@umich.edu>
2018-08-09 15:30:00 -0500

    adding FTM based driver for pwmout_api.c

--- targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_KE15Z/pwmout_api.c
+++ targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_KE15Z/pwmout_api.c
@@ -20,12 +20,12 @@
 
 #include "cmsis.h"
 #include "pinmap.h"
-#include "fsl_tpm.h"
+#include "fsl_ftm.h"
 #include "PeripheralPins.h"
 
 static float pwm_clock_mhz;
-/* Array of TPM peripheral base address. */
-static TPM_Type *const tpm_addrs[] = TPM_BASE_PTRS;
+/* Array of FTM peripheral base address. */
+static FTM_Type *const ftm_addrs[] = FTM_BASE_PTRS;
 
 void pwmout_init(pwmout_t* obj, PinName pin)
 {
@@ -35,10 +35,7 @@ void pwmout_init(pwmout_t* obj, PinName pin)
     obj->pwm_name = pwm;
 
     uint32_t pwm_base_clock;
-
-    /* Set the TPM clock source to be MCG FLL clock */
-    CLOCK_SetTpmClock(1U);
-    pwm_base_clock = CLOCK_GetFreq(kCLOCK_PllFllSelClk);
+    pwm_base_clock = CLOCK_GetFreq(kCLOCK_BusClk);
     float clkval = (float)pwm_base_clock / 1000000.0f;
     uint32_t clkdiv = 0;
     while (clkval > 1) {
@@ -52,22 +49,25 @@ void pwmout_init(pwmout_t* obj, PinName pin)
     pwm_clock_mhz = clkval;
     uint32_t channel = pwm & 0xF;
     uint32_t instance = pwm >> TPM_SHIFT;
-    tpm_config_t tpmInfo;
+    ftm_config_t ftmInfo;
 
-    TPM_GetDefaultConfig(&tpmInfo);
-    tpmInfo.prescale = (tpm_clock_prescale_t)clkdiv;
-    /* Initialize TPM module */
-    TPM_Init(tpm_addrs[instance], &tpmInfo);
+    FTM_GetDefaultConfig(&ftmInfo);
+    ftmInfo.prescale = (ftm_clock_prescale_t)clkdiv;
+    /* Initialize FTM module */
+    FTM_Init(ftm_addrs[instance], &ftmInfo);
 
-    tpm_chnl_pwm_signal_param_t config = {
-        .chnlNumber = (tpm_chnl_t)channel,
-        .level = kTPM_HighTrue,
+    ftm_addrs[instance]->CONF |= FTM_CONF_NUMTOF(3);
+
+    ftm_chnl_pwm_signal_param_t config = {
+        .chnlNumber = (ftm_chnl_t)channel,
+        .level = kFTM_HighTrue,
         .dutyCyclePercent = 0,
+        .firstEdgeDelayPercent = 0
     };
     // default to 20ms: standard for servos, and fine for e.g. brightness control
-    TPM_SetupPwm(tpm_addrs[instance], &config, 1, kTPM_EdgeAlignedPwm, 50, pwm_base_clock);
+    FTM_SetupPwm(ftm_addrs[instance], &config, 1, kFTM_EdgeAlignedPwm, 50, pwm_base_clock);
 
-    TPM_StartTimer(tpm_addrs[instance], kTPM_SystemClock);
+    FTM_StartTimer(ftm_addrs[instance], kFTM_SystemClock);
 
     // Wire pinout
     pinmap_pinout(pin, PinMap_PWM);
@@ -75,7 +75,7 @@ void pwmout_init(pwmout_t* obj, PinName pin)
 
 void pwmout_free(pwmout_t* obj)
 {
-    TPM_Deinit(tpm_addrs[obj->pwm_name >> TPM_SHIFT]);
+    FTM_Deinit(ftm_addrs[obj->pwm_name >> TPM_SHIFT]);
 }
 
 void pwmout_write(pwmout_t* obj, float value)
@@ -86,19 +86,21 @@ void pwmout_write(pwmout_t* obj, float value)
         value = 1.0f;
     }
 
-    TPM_Type *base = tpm_addrs[obj->pwm_name >> TPM_SHIFT];
-    uint16_t mod = base->MOD & TPM_MOD_MOD_MASK;
+    FTM_Type *base = ftm_addrs[obj->pwm_name >> TPM_SHIFT];
+    uint16_t mod = base->MOD & FTM_MOD_MOD_MASK;
     uint32_t new_count = (uint32_t)((float)(mod) * value);
     // Update of CnV register
     base->CONTROLS[obj->pwm_name & 0xF].CnV = new_count;
     base->CNT = 0;
+    /* Software trigger to update registers */
+    FTM_SetSoftwareTrigger(base, true);
 }
 
 float pwmout_read(pwmout_t* obj)
 {
-    TPM_Type *base = tpm_addrs[obj->pwm_name >> TPM_SHIFT];
-    uint16_t count = (base->CONTROLS[obj->pwm_name & 0xF].CnV) & TPM_CnV_VAL_MASK;
-    uint16_t mod = base->MOD & TPM_MOD_MOD_MASK;
+    FTM_Type *base = ftm_addrs[obj->pwm_name >> TPM_SHIFT];
+    uint16_t count = (base->CONTROLS[obj->pwm_name & 0xF].CnV) & FTM_CnV_VAL_MASK;
+    uint16_t mod = base->MOD & FTM_MOD_MOD_MASK;
 
     if (mod == 0)
         return 0.0;
@@ -119,11 +121,11 @@ void pwmout_period_ms(pwmout_t* obj, int ms)
 // Set the PWM period, keeping the duty cycle the same.
 void pwmout_period_us(pwmout_t* obj, int us)
 {
-    TPM_Type *base = tpm_addrs[obj->pwm_name >> TPM_SHIFT];
+    FTM_Type *base = ftm_addrs[obj->pwm_name >> TPM_SHIFT];
     float dc = pwmout_read(obj);
 
-    // Stop TPM clock to ensure instant update of MOD register
-    base->MOD = TPM_MOD_MOD((pwm_clock_mhz * (float)us) - 1);
+    // Stop FTM clock to ensure instant update of MOD register
+    base->MOD = FTM_MOD_MOD((pwm_clock_mhz * (float)us) - 1);
     pwmout_write(obj, dc);
 }
 
@@ -139,11 +141,13 @@ void pwmout_pulsewidth_ms(pwmout_t* obj, int ms)
 
 void pwmout_pulsewidth_us(pwmout_t* obj, int us)
 {
-    TPM_Type *base = tpm_addrs[obj->pwm_name >> TPM_SHIFT];
+    FTM_Type *base = ftm_addrs[obj->pwm_name >> TPM_SHIFT];
     uint32_t value = (uint32_t)(pwm_clock_mhz * (float)us);
 
     // Update of CnV register
     base->CONTROLS[obj->pwm_name & 0xF].CnV = value;
+    /* Software trigger to update registers */
+    FTM_SetSoftwareTrigger(base, true);
 }
 
 #endif
